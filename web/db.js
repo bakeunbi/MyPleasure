5cb5006cc4e05f7417e601d9879ef77e516a381e
Eimmie <bakeunbi1@gmail.com>
2018-08-15 21:06:26 +0900

    add server side
    add server side

---
+++ web/db.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var pool;
+
+exports.connect = function(){
+    pool = mysql.createPool({
+                            connectionLimit:100,
+                            host    : 'localhost',
+                            user    : 'root',
+                            password: 'mobinity',
+                            database: 'myPleasure'
+                            });
+}
+
+exports.get = function(){
+    return pool;
+}
